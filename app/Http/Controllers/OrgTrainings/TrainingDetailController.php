f0dd4a1a7d5a89f30019ebd9e3046865f15b5c88
sedraolapy <sedraolapy25@gmail.com>
2025-09-16 20:48:20 +0300

    org training details v1

--- app/Http/Controllers/OrgTrainings/TrainingDetailController.php
+++ app/Http/Controllers/OrgTrainings/TrainingDetailController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\OrgTrainings;
 
 use App\Http\Controllers\Controller;
+use App\Models\OrgTrainingDetail;
 use Illuminate\Http\Request;
 
 class TrainingDetailController extends Controller
 {
-    
+    public function update($id){
+        $programDetail = OrgTrainingDetail::where('id', $id)->get();
+    }
 }
